a53db95dc8d6236cf6e55ebc6d5160db713c63b7
lanaash <2alanhand@googlemail.com>
2025-07-23 08:17:34 +0100

    Create snmp_ping.pl

---
+++ snmp_ping.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+$ping = "snmpget -v 2c -c public 192.168.0.1 .sysDescr.0";
+
+while (1) {
+    $SIG{INT} = \&signal_handler;
+    system ("$ping > /dev/null 2>&1");
+    if ($? == 0) {
+        # ...we got a reply
+        print "\!";
+    } else{
+        # ...or we didn't
+        print".";
+    }
+
+}
+
+sub signal_handler {
+   die "Caught a signal $!";
+}
